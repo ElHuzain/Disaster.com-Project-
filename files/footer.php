8753a57795471c425a9617ca28b8a2e5b27e0fa2
Ahmed Huzain <98989432+ElHuzain@users.noreply.github.com>
2022-08-18 17:40:18 +0200

    Add files via upload

--- files/footer.php
+++ files/footer.php
@@ -2,7 +2,7 @@
 
 <footer>
     <p style="float:left;">&copy; 2022 Copyrights reserved: Disaster.com</p>
-    <p style="float:right;"><span class="footerInput" onclick="teamOn()">Team</span> | <span class="footerInput" onclick="aboutOn()">About</span></p>
+    <p style="float:right;"><span class="footerInput" onclick="aboutOn()">Team</span> | <span class="footerInput" onclick="teamOn()">About</span></p>
 </footer>
 
 </html>
